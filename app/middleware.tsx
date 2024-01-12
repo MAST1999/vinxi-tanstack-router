f71a3d630012e0a6ac480d136bc421842e7a1f22
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2024-01-13 01:31:51 +0330

    feat: add server function usage :D

--- app/middleware.tsx
+++ app/middleware.tsx
@@ -1,10 +1,11 @@
 import { defineMiddleware, getContext, setContext } from "vinxi/server";
 
 export default defineMiddleware({
-	onRequest: (event) => {
-		setContext(event, "help", { foo: "bar" });
-	},
-	onBeforeResponse: (event) => {
-		console.log(getContext(event, "help"));
-	},
+  onRequest: (event) => {
+    console.log(event.method, event.path);
+    // setContext(event, "help", { foo: "bar" });
+  },
+  onBeforeResponse: (event) => {
+    // console.log(getContext(event, "help"));
+  },
 });
